07118e98bcd4b14ae681c7e661751f86cab4ce79
Avanindra Bose <avanindrabose0218@gmail.com>
2026-07-27 01:36:49 +0530

    Added Messages functionality in the chatbot for maintaining chat history

--- prompt-engineering/chatbot.py
+++ prompt-engineering/chatbot.py
@@ -1,16 +1,19 @@
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage,HumanMessage,AIMessage
 
 model = ChatOpenAI()
 
-chat_history = []
+chat_history = [
+    SystemMessage(content="You are an AI assistant to help me.")
+]
 
 while True:
     user_input = input("You: ")
-    chat_history.append(user_input)
+    chat_history.append(HumanMessage(content=user_input))
     if user_input == "exit":
         break
     result = model.invoke(chat_history)
-    chat_history.append(result.content)
+    chat_history.append(AIMessage(content=result.content))
     print("AI: ",result.content)
 
 print(chat_history)
