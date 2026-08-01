53447011250efdf0d943e729dc3b5fc3532e86f4
Avanindra Bose <avanindrabose0218@gmail.com>
2026-08-01 20:26:33 +0530

    Added Sequnetial Chain

---
+++ Chains/simple_chain.py
@@ -0,0 +1,23 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+template = PromptTemplate(
+    template="Generate 3 interesting facts about {topic}.",
+    input_variables=["topic"]
+)
+
+model = ChatOpenAI()
+
+parser = StrOutputParser()
+
+chain = template | model | parser
+
+result = chain.invoke({"topic":"football"})
+
+print(result)
+
+print(chain.get_graph().draw_ascii())
