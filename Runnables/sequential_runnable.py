34d4d1965c9c6091c3fe465eb8f730abb3917845
Avanindra Bose <avanindrabose0218@gmail.com>
2026-08-05 11:00:30 +0530

    Added Runnable Sequence

---
+++ Runnables/sequential_runnable.py
@@ -0,0 +1,28 @@
+# Runnable Primitives
+from langchain_core.runnables import RunnableSequence
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+parser = StrOutputParser()
+
+prompt1 = PromptTemplate(
+    template = "Write a koke on {topic}",
+    input_variables=["topic"]
+)
+
+prompt2 = PromptTemplate(
+    template="Explain the {joke}",
+    input_variables=["joke"]
+)
+
+chain = RunnableSequence(prompt1,model,parser,prompt2,model,parser)
+
+result = chain.invoke("AI")
+
+print(result)
